8273798aace6e53216ebded6172260cc42d0d96e
Prathmesh <prathamesh.choudhari18@vit.edu>
2021-03-04 21:21:12 +0530

    P7_0103 added.

--- P7_0103/C_solution.cpp
+++ P7_0103/C_solution.cpp
@@ -2,16 +2,29 @@
 
 int main()
 {
-    int n;
+    int n,id=0,maxx=0;
     scanf("%d",&n);
     int arr[n];
-    int extra[100] = {0};
+    int extra[4] = {0};
     for (int i = 0; i < n; i++)
     {
         scanf("%d",arr[i]);
     }
 
+    for (int i=0;i<n;i++)
+    {
+	extra[arr[i]]+=1;
+    }
     
-    
-    
+    for (int i=0;i<4;i++)
+    {
+	if(maxx<extra[i])
+	{
+		maxx = extra[i];
+		id = i;
+	}
+    }
+	
+    printf("\n%d",i);
+      
 }
